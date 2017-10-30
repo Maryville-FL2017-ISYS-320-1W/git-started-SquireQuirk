2b73a0de630d09a364c7f6dfa09864f87223a91b
SquireQuirk <33111232+SquireQuirk@users.noreply.github.com>
2017-10-30 18:44:10 -0400

    completion
    I am pushing the corrections to you-
    
    Patrick Q

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("PATRICK QUIRK");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("BECAUSE I WOULD LIKE TO LEARN JAVA AS A PROGRAMMING LANGUAGE TO BETTER MY UNDERSTANDING OF INTERNET PROTOCOLS");
 	}
 
 }
